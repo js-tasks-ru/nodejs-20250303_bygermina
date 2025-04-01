6681baf4cb270c29f12f9907c89dde06be079efc
Xenia.Liubachka <bygermina@gmail.com>
2025-04-01 20:25:09 +0200

    add logging

--- 03-di/01-notification-service/notifications/notifications.service.ts
+++ 03-di/01-notification-service/notifications/notifications.service.ts
@@ -1,12 +1,21 @@
 import { Injectable, BadRequestException } from "@nestjs/common";
+import { LoggingService } from "../logging/logging.service";
 
 @Injectable()
 export class NotificationsService {
+  constructor(private readonly loggingnService: LoggingService) {}
+
   sendEmail(to: string, subject: string, message: string): void {
     if (!to || !subject || !message) {
       throw new BadRequestException("Invalid email data");
     }
     console.log(`Email sent to ${to}: [${subject}] ${message}`);
+
+    this.loggingnService.logNotification(
+      "Email",
+      to,
+      `[${subject}] ${message}`,
+    );
   }
 
   sendSMS(to: string, message: string): void {
@@ -14,5 +23,7 @@ export class NotificationsService {
       throw new BadRequestException("Invalid SMS data");
     }
     console.log(`SMS sent to ${to}: ${message}`);
+
+    this.loggingnService.logNotification("SMS", to, message);
   }
 }
